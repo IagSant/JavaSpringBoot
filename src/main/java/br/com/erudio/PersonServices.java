0bf458bf94cb984673f845817b5f6a6bbeb78dab
Iago Santos <iagosantos1710@gmail.com>
2025-12-08 20:12:58 -0300

    Commit

--- src/main/java/br/com/erudio/PersonServices.java
+++ src/main/java/br/com/erudio/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.erudio;
 import br.com.erudio.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -12,6 +14,20 @@ public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
 
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll() {
+
+        logger.info("Finding all People!");
+
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 10; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+
 
     public Person findById(String id) {
         logger.info("Finding one Person!");
@@ -24,5 +40,34 @@ public class PersonServices {
         person.setGender("Male");
         return person;
 
+    }
+
+    public Person create(Person person) {
+
+        logger.info("Creating one Person!");
+
+        return person;
+    }
+
+    public Person update(Person person) {
+
+        logger.info("Updating one Person!");
+
+        return person;
+    }
+
+    public void delete(String id) {
+
+        logger.info("Delete one Person!");
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Firstname " + i);
+        person.setLastName("Lastname " + i);
+        person.setAdress("Some Adress in Brasil");
+        person.setGender("Male");
+        return person;
     }
 }
